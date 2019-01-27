1fcb3aff635b22adc1aed28bdaa6e2d9d400f197
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-01-27 15:27:06 +0200

    made w_elf work with w_location

--- GoblinSabotageSquad/w_elf.py
+++ GoblinSabotageSquad/w_elf.py
@@ -1,10 +1,18 @@
 from elf_kingdom import *
-
-
-class elfWR:
+from w_location import LocationWrapper as Location
+class ElfWrapper:
     """
         this class is a wrapper for elf.
     """
 
     def __init__(self, elf):
         self.elf = elf
+
+    def get_location(self):
+        return Location(self.elf.location)
+
+    def get_x(self):
+        return self.get_location().get_x()
+
+    def get_y(self):
+        return self.get_location().get_y()

d01ce43e2108dfb88b7ca87ba9da6d51eecbd8b7
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-01-28 09:11:57 +0200

    added location functions to CastleWrapper

--- GoblinSabotageSquad/w_castle.py
+++ GoblinSabotageSquad/w_castle.py
@@ -1,5 +1,5 @@
 from elf_kingdom import *
-
+from w_location import LocationWrapper
 
 class Castle_Wrapper:
     """
@@ -8,3 +8,12 @@ class Castle_Wrapper:
 
     def __init__(self, castle):
         self.castle = castle
+
+    def get_location(self):
+        return LocationWrapper(self.castle.location)
+
+    def get_x(self):
+        return self.get_location().get_x()
+
+    def get_y(self):
+        return self.get_location().get_y()

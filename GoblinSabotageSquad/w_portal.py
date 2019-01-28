e596c99e5348829be1f76757b6eb493c59985787
Matangn13 <matangn13@gmail.com>
2019-01-28 09:09:28 +0200

    changed doc

--- GoblinSabotageSquad/w_portal.py
+++ GoblinSabotageSquad/w_portal.py
@@ -3,8 +3,9 @@ from elf_kingdom import *
 
 class Portal_Wrapper:
     """
-        this class is a wrapper for castle.
+        this class is a wrapper for portal.
     """
 
-    def __init__(self, portal):
+    def __init__(self, portal, role):
         self.portal = portal
+        self.role = role

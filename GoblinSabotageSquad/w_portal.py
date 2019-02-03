92d998535023cd67edc8bc41bb9763bcdedcf2a6
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-02-03 15:31:44 +0200

    made enemy the default role for portal

--- GoblinSabotageSquad/w_portal.py
+++ GoblinSabotageSquad/w_portal.py
@@ -6,6 +6,6 @@ class PortalWrapper:
         this class is a wrapper for portal.
     """
 
-    def __init__(self, portal, role):
+    def __init__(self, portal, role="enemy"):
         self.portal = portal
         self.role = role

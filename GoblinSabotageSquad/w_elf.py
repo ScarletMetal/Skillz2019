81fe6ca5594e4007c116b09135183a9acd1b0b01
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-02-25 08:49:18 +0200

    added default locations

--- GoblinSabotageSquad/w_elf.py
+++ GoblinSabotageSquad/w_elf.py
@@ -1,6 +1,6 @@
+import location_calculator
 from elf_kingdom import *
 from w_location import LocationWrapper as Location
-import location_calculator
 
 
 class ElfWrapper:
@@ -28,35 +28,41 @@ class ElfWrapper:
     def move_to(self, location):
         self.elf.move_to(location.location)
 
-    def act_attacker(self, enemy_castle, enemy_portals, **context):
-        if self.get_location() == location_calculator.calc_attack_portal_location(enemy_castle,
-                                                                                  self.ATTACK_PORTAL_RADIUS,
-                                                                                  enemy_portals) and self.elf.can_build_portal():
+    def act_attacker(self, enemy_castle, enemy_portals, default_location):
+        target_location = location_calculator.calc_attack_portal_location(enemy_castle,
+                                                                          self.ATTACK_PORTAL_RADIUS,
+                                                                          enemy_portals)
+        if len(enemy_portals) == 0:
+            target_location = default_location
+        if self.get_location() == target_location and self.elf.can_build_portal():
             self.elf.build_portal()
         else:
-            self.move_to(location_calculator.calc_attack_portal_location(enemy_castle, self.ATTACK_PORTAL_RADIUS,
-                                                                         enemy_portals))
+            self.move_to(target_location)
 
-    def act_defender(self, castle, enemy_portals, mana_fountains):
-        if self.get_location() == location_calculator.calc_defense_portal_location(self.DEFENSE_PORTAL_RADIUS,
-                                                                                   castle,
-                                                                                   enemy_portals,
-                                                                                   mana_fountains) and self.elf.can_build_portal():
+    def act_defender(self, castle, enemy_portals, mana_fountains, default_location):
+        target_location = location_calculator.calc_defense_portal_location(self.DEFENSE_PORTAL_RADIUS,
+                                                                           castle,
+                                                                           enemy_portals,
+                                                                           mana_fountains)
+
+        if len(enemy_portals) == 0 or len(mana_fountains) == 0:
+            target_location = default_location
+        if self.get_location() == target_location and self.elf.can_build_portal():
             self.elf.build_portal()
         else:
-            self.move_to(
-                location_calculator.calc_defense_portal_location(self.DEFENSE_PORTAL_RADIUS, castle, enemy_portals,
-                                                                 mana_fountains))
+            self.move_to(target_location)
 
-    def act_mana_fountain(self, castle, enemy_castle, mana_fountains):
-        if self.get_location() == location_calculator.calc_managen_location(self.MANAGEN_RADIUS, castle,
-                                                                            enemy_castle,
-                                                                            mana_fountains) and self.elf.can_build_mana_fountain():
+    def act_mana_fountain(self, castle, enemy_castle, mana_fountains, default_location):
+        target_location = location_calculator.calc_managen_location(self.MANAGEN_RADIUS, castle,
+                                                                    enemy_castle,
+                                                                    mana_fountains)
+
+        if len(mana_fountains) == 0:
+            target_location = default_location
+        if self.get_location() == target_location and self.elf.can_build_mana_fountain():
             self.elf.build_mana_fountain()
         else:
-            self.move_to(location_calculator.calc_managen_location(self.MANAGEN_RADIUS, castle,
-                                                                   enemy_castle,
-                                                                   mana_fountains))
+            self.move_to(target_location)
 
     def act_portal(self):
         pass
